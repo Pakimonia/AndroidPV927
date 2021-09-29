48623a9f04554bdcabd5b06899fba46c1a437eb8
Pakimonia <54326535+Pakimonia@users.noreply.github.com>
2021-09-29 20:50:59 +0300

    base app android, add scroll view and retrofit

---
+++ Android/app/src/main/java/com/example/shopauto/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.shopauto;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText txtInfo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtInfo = findViewById(R.id.txtInfo);
+    }
+    public void onClickInfo(View view){
+
+//        Toast.makeText(this, txtInfo.getText(), Toast.LENGTH_LONG).show();
+        MainActivity intasnce = this;
+        NetworkService.getInstance()
+                .getJSONApi()
+                .getPostWithID(1)
+                .enqueue(new Callback<Post>() {
+                    @Override
+                    public void onResponse(@NonNull Call<Post> call, @NonNull Response<Post> response) {
+                        Post post = response.body();
+
+                        Toast.makeText(intasnce,post.getBody(), Toast.LENGTH_LONG).show();
+//                        textView.append(post.getId() + "\n");
+//                        textView.append(post.getUserId() + "\n");
+//                        textView.append(post.getTitle() + "\n");
+//                        textView.append(post.getBody() + "\n");
+                    }
+
+                    @Override
+                    public void onFailure(@NonNull Call<Post> call, @NonNull Throwable t) {
+
+//                        textView.append("Error occurred while getting request!");
+                        t.printStackTrace();
+                    }
+                });
+
+    }
+}
